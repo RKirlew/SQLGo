aaf6a71343f77dbd593779b2082f97273e9f30d2
R_Kirlew <kirlewraheem@gmail.com>
2023-08-10 23:18:15 -0700

    Update parser.go
    Better comments added, introudcted parsing WHERE clauses, SELECT and FROM basic implementations

--- SQLGo/Parser/parser.go
+++ SQLGo/Parser/parser.go
@@ -33,7 +33,7 @@ type LiteralNode struct {
 	Value string // The literal value
 }
 
-func Parse(fetchedTokens []lexer.Token) Node {
+func Parse(fetchedTokens []lexer.Token) (Node, error) {
 	var rootNode Node
 
 	for i := 0; i < len(fetchedTokens); i++ {
@@ -62,11 +62,25 @@ func Parse(fetchedTokens []lexer.Token) Node {
 
 			case "FROM":
 				// Handle the FROM clause
-				// Create a TableNode or appropriate node type, set relationships, etc.
+
+				selectNode := &SelectNode{}
+				_ = selectNode
+				newIndex, err := parseFromClause(fetchedTokens, i+1, selectNode)
+				if err != nil {
+					return nil, err
+				}
+				i = newIndex
 
 			case "WHERE":
-				// Handle the WHERE clause
-				// Create an appropriate node type, set relationships, etc.
+				selectNode := &SelectNode{}
+				whereNode, newIndex, err := parseWhereClause(fetchedTokens, i+1)
+				if err != nil {
+					return nil, err
+				}
+				i = newIndex - 1 // Decrement by 1 because the loop will increment it
+
+				// Set the WHERE clause node in the SelectNode
+				selectNode.Where = whereNode
 
 			// ... Handle other SQL keywords here ...
 
@@ -88,8 +102,14 @@ func Parse(fetchedTokens []lexer.Token) Node {
 		}
 	}
 
-	return rootNode
+	return rootNode, nil
 }
+
+/*
+The parseColumns function is responsible for parsing and extracting the column names following the "SELECT" keyword in an SQL query.
+
+	It iterates through the list of tokens, identifies column names (identifiers), and adds them to the Columns field of the SelectNode as ColumnNode instances.
+*/
 func parseColumns(tokens []lexer.Token, startIndex int, selectNode *SelectNode) (int, error) {
 	i := startIndex
 
@@ -125,8 +145,19 @@ func parseColumns(tokens []lexer.Token, startIndex int, selectNode *SelectNode) 
 }
 
 func parseFromClause(tokens []lexer.Token, startIndex int, selectNode *SelectNode) (int, error) {
-	// Parse the table reference and create a TableNode
+	// Ensure there are tokens left to parse
+	if startIndex >= len(tokens) {
+		return startIndex, fmt.Errorf("Expected table name, got end of input")
+	}
+
+	// Get the token representing the table name
 	tableToken := tokens[startIndex]
+
+	if tableToken.Type != "IDENTIFIER" {
+		return startIndex, fmt.Errorf("Expected table name, got %s", tableToken.Value)
+	}
+
+	// Create a TableNode for the table reference
 	tableNode := &TableNode{Name: tableToken.Value}
 
 	// Set the TableNode as the From field of the SelectNode
@@ -134,4 +165,76 @@ func parseFromClause(tokens []lexer.Token, startIndex int, selectNode *SelectNod
 
 	// Return the index after parsing the FROM clause
 	return startIndex + 1, nil
+}
+
+func parseWhereClause(tokens []lexer.Token, startIndex int) (Node, int, error) {
+	// Initialize variables to keep track of parsing progress
+	var currentNode Node
+	i := startIndex
+
+	// Loop through tokens starting from startIndex
+	for i < len(tokens) {
+		token := tokens[i]
+
+		switch token.Type {
+		case "IDENTIFIER":
+			// Handle identifier (left operand of the operator)
+			leftOperand := &ColumnNode{Name: token.Value}
+
+			// Move to the next token
+			i++
+
+			// Expect the next token to be an operator
+			if i >= len(tokens) {
+				return nil, i, fmt.Errorf("Expected operator, got end of input")
+			}
+			operatorToken := tokens[i]
+
+			if operatorToken.Type != "OPERATOR" {
+				return nil, i, fmt.Errorf("Expected operator, got %s", operatorToken.Value)
+			}
+
+			// Handle operator
+			operator := &OperatorNode{Operator: operatorToken.Value, Left: leftOperand}
+
+			// Move to the next token
+			i++
+
+			// Expect the next token to be a literal
+			if i >= len(tokens) {
+				return nil, i, fmt.Errorf("Expected literal, got end of input")
+			}
+			literalToken := tokens[i]
+
+			if literalToken.Type != "LITERAL" {
+				return nil, i, fmt.Errorf("Expected literal, got %s", literalToken.Value)
+			}
+
+			// Handle literal (right operand of the operator)
+			rightOperand := &LiteralNode{Value: literalToken.Value}
+
+			// Set the right operand for the operator node
+			operator.Right = rightOperand
+
+			// Set the operator node as the current node
+			currentNode = operator
+
+			// Move to the next token
+			i++
+
+			// If the next token is AND or OR, continue parsing the next expression
+			if i < len(tokens) && (tokens[i].Value == "AND" || tokens[i].Value == "OR") {
+				// TODO: Handle AND / OR logic
+				// For now, simply move to the next token
+				i++
+			}
+
+		default:
+			// Handle syntax errors or other cases
+			return nil, i, fmt.Errorf("Expected identifier, got %s", token.Value)
+		}
+	}
+
+	// Return the parsed WHERE clause node and the new index
+	return currentNode, i, nil
 }
